777a3832cec43baa825774ee6bdf5b03e14e6cb0
ararso1 <arasoalisho2@gmail.com>
2025-02-03 16:32:52 +0300

    update payment models

--- addons/custom_modules/payment_chapa/models/payment_provider.py
+++ addons/custom_modules/payment_chapa/models/payment_provider.py
@@ -6,8 +6,8 @@ from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 import json
 import base64
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_v1_5
+# from Crypto.PublicKey import RSA
+# from Crypto.Cipher import PKCS1_v1_5
 import binascii
 from odoo.exceptions import AccessError
 
