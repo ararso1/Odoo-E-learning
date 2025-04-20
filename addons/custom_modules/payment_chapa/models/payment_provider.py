5bdac020b934df430980e5914e8f698ec594ac62
ararso1 <82933201+ararso1@users.noreply.github.com>
2025-04-20 17:44:35 +0300

    Update payment_provider.py
    update to payment number

--- addons/custom_modules/payment_chapa/models/payment_provider.py
+++ addons/custom_modules/payment_chapa/models/payment_provider.py
@@ -93,8 +93,8 @@ class PaymentProvider(models.Model):
                 "tx_ref": "",
                 "currency": self.encryptor("ETB"),
                 "first_name": "haile",
-                "email": self.encryptor("abdulselam4246@gmail.com"),
-                "phone_number":self.encryptor(data['partner_phone']),
+                "email": self.encryptor("areealisho12@gmail.com"),
+                "phone_number":self.encryptor('0912000000'),
                 "last_name": "tsega",
                 "customization": {
                     "title": "Payment",
