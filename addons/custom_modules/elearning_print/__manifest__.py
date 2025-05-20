9297cc67cfa03691a8be4f0aaaf0ef3d252d2af1
Abdulselam Molla <abdulselam4246@gmail.com>
2025-05-20 14:55:43 +0300

    cleaningup

--- addons/custom_modules/elearning_print/__manifest__.py
+++ addons/custom_modules/elearning_print/__manifest__.py
@@ -2,9 +2,6 @@
     "name":"Elearning Printout Course",
     "depends":["base","survey"],
     "data":[
-        "report/survey_report.xml",
-        "report/reports.xml",
-        #"report/report_elearning_template.xml",
         "report/survey_template_direct.xml"
     ]
 }
