b8643b7c4b92b9401b2449db56b480ee7d83228a
aosqa <abdulselam4246@gmail.com>
2025-06-23 11:22:18 +0300

    Update __manifest__.py

--- addons/custom_modules/elearning_print/__manifest__.py
+++ addons/custom_modules/elearning_print/__manifest__.py
@@ -1,7 +1,13 @@
 {
     "name":"Elearning Printout Course",
-    "depends":["base","survey"],
+    "depends": ["base", "survey", "web"],
     "data":[
         "report/survey_template_direct.xml"
-    ]
+    ],
+    'assets': {
+        'web.report_assets_common': [  # ✅ use correct bundle name
+            ('remove', 'survey/static/src/scss/survey_reports.scss'),
+            'elearning_print/static/src/scss/survey_reports.scss',
+        ],
+    },
 }
